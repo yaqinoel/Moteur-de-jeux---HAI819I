896deb53330e3c63da7a2c7c335805fd0104b68e
yaqinoel <yaqizhang.unique@outlook.com>
2026-04-14 18:43:47 +0200

    Changed Data log system

--- TP_Moteur_Base/common/DataLogger.hpp
+++ TP_Moteur_Base/common/DataLogger.hpp
@@ -3,18 +3,26 @@
 #include "GameObjet.hpp"
 #include "SimulationData.hpp"
 
+const glm::vec3 GRAVITY = glm::vec3(0.0f, -9.81f, 0.0f);
+
 class DataLogger {
 public:
-    DataLogger(const GameObjet * target) : m_target(target) {}
+    DataLogger(const GameObjet * target) : m_target(target) {
+        if (target != NULL) {
+            startPos = target->physicsModel->m_physicsPosition;
+            startVel = target->physicsModel->m_velocity;
+        }
+    }
 
     void sample(float deltaTime) {
         if (m_target) {
             auto p = m_target->physicsModel->m_physicsPosition;
             auto v = m_target->physicsModel->m_velocity;
+            glm::vec3 p_a = getAnalytiquePosition();
+            glm::vec3 v_a =  getAnalytiqueVelocity();
             m_data.addRow(
                 current_time,
-                p.x,p.y,p.z,
-                v.x,v.y,v.z
+                p,v,p_a,v_a
             );
             current_time+= deltaTime;
         }
@@ -24,8 +32,20 @@ public:
         m_data.saveToCSV(filename);
     }
 
+    glm::vec3 getAnalytiqueVelocity() {
+        glm::vec3 va(0.f);
+        return va = startVel + current_time * GRAVITY;
+    }
+
+    glm::vec3 getAnalytiquePosition() {
+        glm::vec3 pa(0.f);
+        return pa = startPos + startVel * current_time + (1/2.f) * GRAVITY * current_time * current_time;
+    }
+
 private:
     const GameObjet* m_target;
+    glm::vec3 startPos;
+    glm::vec3 startVel;
     SimulationData m_data;
     float current_time = 0.0f;
 };
--- TP_Moteur_Base/common/SimulationData.hpp
+++ TP_Moteur_Base/common/SimulationData.hpp
@@ -11,11 +11,18 @@ struct DataRow {
     float time;
     float px, py, pz;
     float vx, vy, vz;
+    float px_a, py_a, pz_a;
+    float vx_a, vy_a, vz_a;
 };
 class SimulationData {
 public:
-    void addRow(float t, float px, float py, float pz, float vx, float vy, float vz) {
-        m_rows.push_back({t, px, py, pz, vx, vy, vz});
+    void addRow(float t, glm::vec3 pos, glm::vec3 vel, glm::vec3 pos_a, glm::vec3 vel_a) {
+        m_rows.push_back({t,
+            pos.x, pos.y, pos.z,
+            vel.x, vel.y, vel.z,
+            pos_a.x, pos_a.y, pos_a.z,
+            vel_a.x, vel_a.y, vel_a.z
+        });
     }
 
     void saveToCSV(const std::string& filename) {
@@ -26,12 +33,14 @@ public:
             return;
         }
 
-        file << "Time,PosX,PosY,PosZ,VelX,VelY,VelZ\n";
+        file << "Time,PosX,PosY,PosZ,VelX,VelY,VelZ,PosX_A,PosY_A,PosZ_A,VelX_A,VelY_A,VelZ_A\n";
 
         for (const auto& row : m_rows) {
             file << row.time << ","
                  << row.px << "," << row.py << "," << row.pz << ","
-                 << row.vx << "," << row.vy << "," << row.vz << "\n";
+                 << row.vx << "," << row.vy << "," << row.vz << ","
+                 << row.px_a << "," << row.py_a << "," << row.pz_a << ","
+                 << row.vx_a << "," << row.vy_a << "," << row.vz_a << "\n";
         }
 
         file.close();
